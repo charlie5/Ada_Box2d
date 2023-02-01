e64c615b4216a7e85e197fa74591849dde1200c6
Rod Kay <rodakay5@gmail.com>
2023-02-01 18:05:27 +1100

    Initial project skeleton.

---
+++ applet/demo/hello_box2d/source/launch_hello_box2d.adb
@@ -0,0 +1,9 @@
+with
+     Box2d;
+
+
+procedure launch_hello_Box2d
+is
+begin
+   null;
+end launch_hello_Box2d;
---
+++ library/source/box2d.ads
@@ -0,0 +1,10 @@
+package Box2d
+   with Pure
+--
+-- Top level namespace for the Ada port of Box2d.
+--
+is
+
+
+
+end Box2d;
