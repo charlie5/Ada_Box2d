fb66df59b5b9d1036f4e50f491ee3a2b9ec26b7e
Rod Kay <rodakay5@gmail.com>
2023-02-02 00:02:22 +1100

    Port b2_Types.

--- library/source/private/b2_types.ads
+++ library/source/private/b2_types.ads
@@ -1,12 +1,24 @@
+with
+     Interfaces;
+
 
 package b2_Types
 is
+   --  typedef signed char  int8;
+   --  typedef signed short int16;
+   --  typedef signed int   int32;
 
-   --  typedef signed char int8;
---  typedef signed short int16;
---  typedef signed int int32;
---  typedef unsigned char uint8;
---  typedef unsigned short uint16;
---  typedef unsigned int uint32;
---
+   subtype int8  is interfaces.Integer_8;
+   subtype int16 is interfaces.Integer_16;
+   subtype int32 is interfaces.Integer_32;
+
+
+   --  typedef unsigned char  uint8;
+   --  typedef unsigned short uint16;
+   --  typedef unsigned int   uint32;
+
+   subtype uint8  is interfaces.Unsigned_8;
+   subtype uint16 is interfaces.Unsigned_16;
+   subtype uint32 is interfaces.Unsigned_32;
+
 end b2_Types;
