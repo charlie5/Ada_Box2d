e74a467927e6e2da33ed3b40549633a6a5d70bc4
Rod Kay <rodakay5@gmail.com>
2023-02-05 21:26:14 +1100

    Partial port of 'b2_Shape' spec.

--- library/source/shapes/b2_shape.ads
+++ library/source/shapes/b2_shape.ads
@@ -1,90 +1,160 @@
 with
      b2_Math,
-     b2_Collision;
+     b2_Collision,
+     b2_block_Allocator;
 
 
 package b2_Shape
 is
 
+   --  class b2BlockAllocator;
    --
---  class b2BlockAllocator;
---
---  /// This holds the mass data computed for a shape.
---  struct b2MassData
---  {
---    /// The mass of the shape, usually in kilograms.
---    float mass;
---
---    /// The position of the shape's centroid relative to the shape's origin.
---    b2Vec2 center;
---
---    /// The rotational inertia of the shape about the local origin.
---    float I;
---  };
---
---  /// A shape is used for collision detection. You can create a shape however you like.
---  /// Shapes used for simulation in b2World are created automatically when a b2Fixture
---  /// is created. Shapes may encapsulate a one or more child shapes.
---  class b2Shape
---  {
---  public:
---
---    enum Type
---    {
---       e_circle = 0,
---       e_edge = 1,
---       e_polygon = 2,
---       e_chain = 3,
---       e_typeCount = 4
---    };
---
---    virtual ~b2Shape() {}
---
---    /// Clone the concrete shape using the provided allocator.
---    virtual b2Shape* Clone(b2BlockAllocator* allocator) const = 0;
---
---    /// Get the type of this shape. You can use this to down cast to the concrete shape.
---    /// @return the shape type.
---    Type GetType() const;
---
---    /// Get the number of child primitives.
---    virtual int32 GetChildCount() const = 0;
---
---    /// Test a point for containment in this shape. This only works for convex shapes.
---    /// @param xf the shape world transform.
---    /// @param p a point in world coordinates.
---    virtual bool TestPoint(const b2Transform& xf, const b2Vec2& p) const = 0;
---
---    /// Cast a ray against a child shape.
---    /// @param output the ray-cast results.
---    /// @param input the ray-cast input parameters.
---    /// @param transform the transform to be applied to the shape.
---    /// @param childIndex the child shape index
---    virtual bool RayCast(b2RayCastOutput* output, const b2RayCastInput& input,
---                   const b2Transform& transform, int32 childIndex) const = 0;
---
---    /// Given a transform, compute the associated axis aligned bounding box for a child shape.
---    /// @param aabb returns the axis aligned box.
---    /// @param xf the world transform of the shape.
---    /// @param childIndex the child shape
---    virtual void ComputeAABB(b2AABB* aabb, const b2Transform& xf, int32 childIndex) const = 0;
---
---    /// Compute the mass properties of this shape using its dimensions and density.
---    /// The inertia tensor is computed about the local origin.
---    /// @param massData returns the mass data for this shape.
---    /// @param density the density in kilograms per meter squared.
---    virtual void ComputeMass(b2MassData* massData, float density) const = 0;
---
---    Type m_type;
---
---    /// Radius of a shape. For polygonal shapes this must be b2_polygonRadius. There is no support for
---    /// making rounded polygons.
---    float m_radius;
---  };
---
---  inline b2Shape::Type b2Shape::GetType() const
---  {
---    return m_type;
---  }
---
+
+   use b2_Math,
+       b2_block_Allocator;
+
+
+
+   -------------
+   -- b2MassData
+   --
+
+   --  This holds the mass data computed for a shape.
+   --
+
+   --  struct b2MassData
+   --  {
+   --    /// The mass of the shape, usually in kilograms.
+   --    float mass;
+   --
+   --    /// The position of the shape's centroid relative to the shape's origin.
+   --    b2Vec2 center;
+   --
+   --    /// The rotational inertia of the shape about the local origin.
+   --    float I;
+   --  };
+
+   type b2massData is
+      record
+         Mass   : Real;       -- The mass of the shape, usually in kilograms.
+         Center : b2Vec2;     -- The position of the shape's centroid relative to the shape's origin.
+         I      : Real;       -- The rotational inertia of the shape about the local origin.
+      end record;
+
+
+
+
+   ----------
+   -- b2Shape
+   --
+
+   --  A shape is used for collision detection. You can create a shape however you like.
+   --  Shapes used for simulation in b2World are created automatically when a b2Fixture
+   --  is created. Shapes may encapsulate a one or more child shapes.
+   --
+
+   --    enum Type
+   --    {
+   --       e_circle = 0,
+   --       e_edge = 1,
+   --       e_polygon = 2,
+   --       e_chain = 3,
+   --       e_typeCount = 4
+   --    };
+
+   type shape_Type is (e_circle,
+                       e_edge,
+                       e_polygon,
+                       e_chain,
+                       e_typeCount);
+
+   for shape_Type use (e_circle    => 0,
+                       e_edge      => 1,
+                       e_polygon   => 2,
+                       e_chain     => 3,
+                       e_typeCount => 4);
+
+   --  class b2Shape
+   --  {
+   --  public:
+   --
+   --
+   --    Type m_type;
+   --
+   --    /// Radius of a shape. For polygonal shapes this must be b2_polygonRadius. There is no support for
+   --    /// making rounded polygons.
+   --    float m_radius;
+   --  };
+
+
+   type b2Shape is abstract tagged
+      record
+         m_Type   : shape_Type;
+         m_Radius : Real;
+      end record;
+
+   type b2Shape_view is access all b2Shape'Class;
+
+
+
+   --    virtual ~b2Shape() {}
+   --
+
+   procedure destruct (Self : in out b2Shape) is null;
+
+
+
+   --    Clone the concrete shape using the provided allocator.
+   --
+   --    virtual b2Shape* Clone(b2BlockAllocator* allocator) const = 0;
+   --
+
+   function clone (Self : in b2Shape;   Allocator : in out b2BlockAllocator) return b2Shape_view
+                   is abstract;
+
+
+
+   --    /// Get the type of this shape. You can use this to down cast to the concrete shape.
+   --    /// @return the shape type.
+   --    Type GetType() const;
+
+
+   --    /// Get the number of child primitives.
+   --    virtual int32 GetChildCount() const = 0;
+   --
+   --    /// Test a point for containment in this shape. This only works for convex shapes.
+   --    /// @param xf the shape world transform.
+   --    /// @param p a point in world coordinates.
+   --    virtual bool TestPoint(const b2Transform& xf, const b2Vec2& p) const = 0;
+
+
+   --    /// Cast a ray against a child shape.
+   --    /// @param output the ray-cast results.
+   --    /// @param input the ray-cast input parameters.
+   --    /// @param transform the transform to be applied to the shape.
+   --    /// @param childIndex the child shape index
+   --    virtual bool RayCast(b2RayCastOutput* output, const b2RayCastInput& input,
+   --                   const b2Transform& transform, int32 childIndex) const = 0;
+
+
+   --    /// Given a transform, compute the associated axis aligned bounding box for a child shape.
+   --    /// @param aabb returns the axis aligned box.
+   --    /// @param xf the world transform of the shape.
+   --    /// @param childIndex the child shape
+   --    virtual void ComputeAABB(b2AABB* aabb, const b2Transform& xf, int32 childIndex) const = 0;
+
+
+   --    /// Compute the mass properties of this shape using its dimensions and density.
+   --    /// The inertia tensor is computed about the local origin.
+   --    /// @param massData returns the mass data for this shape.
+   --    /// @param density the density in kilograms per meter squared.
+   --    virtual void ComputeMass(b2MassData* massData, float density) const = 0;
+
+
+
+   --  inline b2Shape::Type b2Shape::GetType() const
+   --  {
+   --    return m_type;
+   --  }
+   --
 end b2_Shape;
