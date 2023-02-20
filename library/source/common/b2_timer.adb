c86a3a08efba6af72da272c0f2a951959e0a5e62
Rod Kay <rodakay5@gmail.com>
2023-02-20 23:20:03 +1100

    Port 'b2_Timer'.

--- library/source/common/b2_timer.adb
+++ library/source/common/b2_timer.adb
@@ -1,112 +1,154 @@
-with
-     b2_Timer;
-
-
 package body b2_Timer
 is
-   procedure dummy is null;
-
-
+   --    Constructor
    --
---  #if defined(_WIN32)
---
---  double b2Timer::s_invFrequency = 0.0;
---
---  #ifndef WIN32_LEAN_AND_MEAN
---  #define WIN32_LEAN_AND_MEAN
---  #endif
---
---  #include <windows.h>
---
---  b2Timer::b2Timer()
---  {
---    LARGE_INTEGER largeInteger;
---
---    if (s_invFrequency == 0.0)
---    {
---       QueryPerformanceFrequency(&largeInteger);
---       s_invFrequency = double(largeInteger.QuadPart);
---       if (s_invFrequency > 0.0)
---       {
---          s_invFrequency = 1000.0 / s_invFrequency;
---       }
---    }
---
---    QueryPerformanceCounter(&largeInteger);
---    m_start = double(largeInteger.QuadPart);
---  }
---
---  void b2Timer::Reset()
---  {
---    LARGE_INTEGER largeInteger;
---    QueryPerformanceCounter(&largeInteger);
---    m_start = double(largeInteger.QuadPart);
---  }
---
---  float b2Timer::GetMilliseconds() const
---  {
---    LARGE_INTEGER largeInteger;
---    QueryPerformanceCounter(&largeInteger);
---    double count = double(largeInteger.QuadPart);
---    float ms = float(s_invFrequency * (count - m_start));
---    return ms;
---  }
---
---  #elif defined(__linux__) || defined (__APPLE__)
---
---  #include <sys/time.h>
---
---  b2Timer::b2Timer()
---  {
---      Reset();
---  }
---
---  void b2Timer::Reset()
---  {
---      timeval t;
---      gettimeofday(&t, 0);
---      m_start_sec = t.tv_sec;
---      m_start_usec = t.tv_usec;
---  }
---
---  float b2Timer::GetMilliseconds() const
---  {
---      timeval t;
---      gettimeofday(&t, 0);
---    time_t start_sec = m_start_sec;
---    suseconds_t start_usec = m_start_usec;
---
---    // http://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
---    if (t.tv_usec < start_usec)
---    {
---       int nsec = (start_usec - t.tv_usec) / 1000000 + 1;
---       start_usec -= 1000000 * nsec;
---       start_sec += nsec;
---    }
---
---    if (t.tv_usec - start_usec > 1000000)
---    {
---       int nsec = (t.tv_usec - start_usec) / 1000000;
---       start_usec += 1000000 * nsec;
---       start_sec -= nsec;
---    }
---    return 1000.0f * (t.tv_sec - start_sec) + 0.001f * (t.tv_usec - start_usec);
---  }
---
---  #else
---
---  b2Timer::b2Timer()
---  {
---  }
---
---  void b2Timer::Reset()
---  {
---  }
---
---  float b2Timer::GetMilliseconds() const
---  {
---    return 0.0f;
---  }
---
---  #endif
+   --    b2Timer();
+   --
+   --  b2Timer::b2Timer()
+   --  {
+   --      Reset();
+   --  }
+   --
+
+   function to_b2Timer return b2Timer
+   is
+      Self : b2Timer;
+   begin
+      Self.reset;
+      return Self;
+   end to_b2Timer;
+
+
+   --  float b2Timer::GetMilliseconds() const
+   --  {
+   --      timeval t;
+   --      gettimeofday(&t, 0);
+   --    time_t start_sec = m_start_sec;
+   --    suseconds_t start_usec = m_start_usec;
+   --
+   --    // http://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
+   --    if (t.tv_usec < start_usec)
+   --    {
+   --       int nsec = (start_usec - t.tv_usec) / 1000000 + 1;
+   --       start_usec -= 1000000 * nsec;
+   --       start_sec += nsec;
+   --    }
+   --
+   --    if (t.tv_usec - start_usec > 1000000)
+   --    {
+   --       int nsec = (t.tv_usec - start_usec) / 1000000;
+   --       start_usec += 1000000 * nsec;
+   --       start_sec -= nsec;
+   --    }
+   --    return 1000.0f * (t.tv_sec - start_sec) + 0.001f * (t.tv_usec - start_usec);
+   --  }
+
+
+
+
+   --    Reset the timer.
+   --
+   --    void Reset();
+   --
+   --  void b2Timer::Reset()
+   --  {
+   --      timeval t;
+   --      gettimeofday(&t, 0);
+   --      m_start_sec = t.tv_sec;
+   --      m_start_usec = t.tv_usec;
+   --  }
+   --
+
+   procedure reset (Self : out b2Timer)
+   is
+   begin
+      Self.Start := ada.Calendar.Clock;
+   end reset;
+
+
+
+
+   --    Get the time since construction or the last reset.
+   --
+   --    float GetMilliseconds() const;
+   --
+
+   function getMilliseconds (Self : in b2Timer) return Real
+   is
+      use ada.Calendar;
+   begin
+      return Real ((ada.Calendar.Clock - Self.Start) / 1_000.0);
+   end getMilliseconds;
+
+
+
+
+
+   --  #if defined(_WIN32)
+   --
+   --  double b2Timer::s_invFrequency = 0.0;
+   --
+   --  #ifndef WIN32_LEAN_AND_MEAN
+   --  #define WIN32_LEAN_AND_MEAN
+   --  #endif
+   --
+   --  #include <windows.h>
+   --
+   --  b2Timer::b2Timer()
+   --  {
+   --    LARGE_INTEGER largeInteger;
+   --
+   --    if (s_invFrequency == 0.0)
+   --    {
+   --       QueryPerformanceFrequency(&largeInteger);
+   --       s_invFrequency = double(largeInteger.QuadPart);
+   --       if (s_invFrequency > 0.0)
+   --       {
+   --          s_invFrequency = 1000.0 / s_invFrequency;
+   --       }
+   --    }
+   --
+   --    QueryPerformanceCounter(&largeInteger);
+   --    m_start = double(largeInteger.QuadPart);
+   --  }
+   --
+   --  void b2Timer::Reset()
+   --  {
+   --    LARGE_INTEGER largeInteger;
+   --    QueryPerformanceCounter(&largeInteger);
+   --    m_start = double(largeInteger.QuadPart);
+   --  }
+   --
+   --  float b2Timer::GetMilliseconds() const
+   --  {
+   --    LARGE_INTEGER largeInteger;
+   --    QueryPerformanceCounter(&largeInteger);
+   --    double count = double(largeInteger.QuadPart);
+   --    float ms = float(s_invFrequency * (count - m_start));
+   --    return ms;
+   --  }
+
+
+   --  #elif defined(__linux__) || defined (__APPLE__)
+   --
+   --  #include <sys/time.h>
+   --
+
+
+   --  #else
+   --
+   --  b2Timer::b2Timer()
+   --  {
+   --  }
+   --
+   --  void b2Timer::Reset()
+   --  {
+   --  }
+   --
+   --  float b2Timer::GetMilliseconds() const
+   --  {
+   --    return 0.0f;
+   --  }
+   --
+   --  #endif   // _WIN32
 end b2_Timer;
--- library/source/common/b2_timer.ads
+++ library/source/common/b2_timer.ads
@@ -1,37 +1,65 @@
 with
-     b2_Settings;
+     b2_Settings,
+     ada.Calendar,
+     Interfaces;
 
 
 package b2_Timer
+--
+--  Timer for profiling. This has platform specific code and may
+--  not work on every platform.
+--
 is
-   procedure dummy;
+   use b2_Settings,
+       Interfaces;
 
 
+
+   --  class b2Timer
+   --  {
+   --  public:
    --
---  /// Timer for profiling. This has platform specific code and may
---  /// not work on every platform.
---  class b2Timer
---  {
---  public:
---
---    /// Constructor
---    b2Timer();
---
---    /// Reset the timer.
---    void Reset();
---
---    /// Get the time since construction or the last reset.
---    float GetMilliseconds() const;
---
---  private:
---
---  #if defined(_WIN32)
---    double m_start;
---    static double s_invFrequency;
---  #elif defined(__linux__) || defined (__APPLE__)
---    unsigned long long m_start_sec;
---    unsigned long long m_start_usec;
---  #endif
---  };
---
+   --  private:
+   --
+   --  #if defined(_WIN32)
+   --    double m_start;
+   --    static double s_invFrequency;
+   --  #elif defined(__linux__) || defined (__APPLE__)
+   --    unsigned long long m_start_sec;
+   --    unsigned long long m_start_usec;
+   --  #endif
+   --  };
+   --
+
+   type b2Timer is tagged
+      record
+         Start : ada.Calendar.Time;
+      end record;
+
+
+   --    Constructor
+   --
+   --    b2Timer();
+   --
+   function to_b2Timer return b2Timer;
+
+
+
+   --    Reset the timer.
+   --
+   --    void Reset();
+   --
+
+   procedure reset (Self : out b2Timer);
+
+
+
+   --    Get the time since construction or the last reset.
+   --
+   --    float GetMilliseconds() const;
+   --
+
+   function getMilliseconds (Self : in b2Timer) return Real;
+
+
 end b2_Timer;
